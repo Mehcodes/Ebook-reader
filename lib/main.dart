b43837116aa172760eccd01ab75bc47d9d0a7d7b
Mehmet <mehmet007@Mehmets-MacBook-Pro.local>
2023-07-02 18:13:28 +0300

    add hive db

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,16 @@ import 'package:ebookapp/color.dart';
 
 // Importing the material.dart file from the Flutter framework.
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 // Importing the 'homepage.dart' file from the current package.
 import 'homepage.dart';
 
 // The entry point of the application.
-void main() {
-  // Runs the MyApp widget as the root of the application.
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
   runApp(const MyApp());
 }
 
@@ -25,22 +28,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // Disables the debug banner on the top-right corner of the app.
       debugShowCheckedModeBanner: false,
-      
+
       // Sets the title of the application.
       title: 'ebook app',
-      
+
       // Configuring the theme of the application.
       theme: ThemeData(
         // Sets the default font family for the app.
         fontFamily: 'Poppins',
-        
+
         // Sets the background color to the KPrimaryColor defined in 'color.dart'.
         backgroundColor: KPrimaryColor,
       ),
-      
+
       // Sets the home page of the application to the BottomNavBar widget.
       home: const BottomNavBar(),
     );
   }
 }
-
