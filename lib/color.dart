e335ff43ef2cb425466579db687764a6fefbc961
Son of Violet <40169771+Mehcodes@users.noreply.github.com>
2023-07-10 15:11:58 +0300

    Update color.dart

--- lib/color.dart
+++ lib/color.dart
@@ -12,7 +12,7 @@ const KSecondaryColor = Color(0xFF326789);
 const KThirdColor = Color(0xFF78A6CB);
 
 // Defining a constant variable 'KFourthColor' with a value of Color.fromARGB(218, 221, 41, 25).
-const KFourthColor = Color.fromARGB(218, 221, 41, 25);
+const KFourthColor = Color.fromARGB(218, 173, 21, 109);
 
 // Defining a constant variable 'KFifthColor' with a value of Color(0xFF484848).
 const KFifthColor = Color(0xFF484848);
